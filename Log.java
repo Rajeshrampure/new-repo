4ea74445bf714e2e96d4cc0ccd20be85330f3184
Rajeshrampure <rajeshrampure1997@gmail.com>
2023-01-01 12:40:10 +0530

    Log class commited method added

--- Log.java
+++ Log.java
@@ -1,6 +1,12 @@
 public class Log {
 
+    // author: Rajesh
+    // New method added
+   static void show(){
+       System.out.println("Show");
+   }
     public static void main(String[] args) {
         System.out.println("Hell");
+        show();
     }
 }
